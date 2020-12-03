3d73287f111704c4808487324b8903d63ffce0e7
Ruige Lee <295054118@qq.com>
2020-12-03 19:59:44 +0800

    mux ifu to icache

--- core/axi/inner_crossBar.v
+++ core/axi/inner_crossBar.v
@@ -4,7 +4,7 @@
 * @Email: wut.ruigeli@gmail.com
 * @Date:   2020-12-02 14:51:36
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2020-12-02 18:00:27
+* @Last Modified time: 2020-12-03 19:57:24
 */
 
 
@@ -156,31 +156,36 @@ module inner_crossBar (
 	input S_IFU_AXI_ARVALID,
 	output S_IFU_AXI_ARREADY,
 
-	output [63:0] S_IFU_AXI_RDATA,
+	output [31:0] S_IFU_AXI_RDATA,
 	output [1:0] S_IFU_AXI_RRESP,
 	output S_IFU_AXI_RVALID,
 	input S_IFU_AXI_RREADY,
 
-//LSU
 
-	input [7:0] S_LSU_AXI_AWID,
+// LLLLLLLLLLL                SSSSSSSSSSSSSSS UUUUUUUU     UUUUUUUU
+// L:::::::::L              SS:::::::::::::::SU::::::U     U::::::U
+// L:::::::::L             S:::::SSSSSS::::::SU::::::U     U::::::U
+// LL:::::::LL             S:::::S     SSSSSSSUU:::::U     U:::::UU
+//   L:::::L               S:::::S             U:::::U     U:::::U 
+//   L:::::L               S:::::S             U:::::D     D:::::U 
+//   L:::::L                S::::SSSS          U:::::D     D:::::U 
+//   L:::::L                 SS::::::SSSSS     U:::::D     D:::::U 
+//   L:::::L                   SSS::::::::SS   U:::::D     D:::::U 
+//   L:::::L                      SSSSSS::::S  U:::::D     D:::::U 
+//   L:::::L                           S:::::S U:::::D     D:::::U 
+//   L:::::L         LLLLLL            S:::::S U::::::U   U::::::U 
+// LL:::::::LLLLLLLLL:::::LSSSSSSS     S:::::S U:::::::UUU:::::::U 
+// L::::::::::::::::::::::LS::::::SSSSSS:::::S  UU:::::::::::::UU  
+// L::::::::::::::::::::::LS:::::::::::::::SS     UU:::::::::UU    
+// LLLLLLLLLLLLLLLLLLLLLLLL SSSSSSSSSSSSSSS         UUUUUUUUU      
+
+
 	input [63:0] S_LSU_AXI_AWADDR,
-	input [7:0] S_LSU_AXI_AWLEN,
-	input [2:0] S_LSU_AXI_AWSIZE,
-	input [1:0] S_LSU_AXI_AWBURST,
-	// input S_LSU_AXI_AWLOCK,
-	// input [3:0] S_LSU_AXI_AWCACHE,
-	// input [2:0] S_LSU_AXI_AWPROT,
-	// input [3:0] S_LSU_AXI_AWQOS,
-	// input [3:0] S_LSU_AXI_AWREGION,
-	input [7:0] S_LSU_AXI_AWUSER,
 	input S_LSU_AXI_AWVALID,
 	output S_LSU_AXI_AWREADY,
 
 	input [63:0] S_LSU_AXI_WDATA,
 	input [7:0] S_LSU_AXI_WSTRB,
-	input S_LSU_AXI_WLAST,
-	input [7:0] S_LSU_AXI_WUSER,
 	input S_LSU_AXI_WVALID,
 	output S_LSU_AXI_WREADY,
 
@@ -296,7 +301,7 @@ module inner_crossBar (
 	output M_ICACHE_AXI_ARVALID,
 	input M_ICACHE_AXI_ARREADY,
 
-	input [63 0] M_ICACHE_AXI_RDATA,
+	input [31:0] M_ICACHE_AXI_RDATA,
 	input [1:0] M_ICACHE_AXI_RRESP,
 	input M_ICACHE_AXI_RVALID,
 	output M_ICACHE_AXI_RREADY,
@@ -305,70 +310,100 @@ module inner_crossBar (
 
 	input dcache_enable,
 
-	output [7:0] M_DCACHE_AXI_AWID,
 	output [63:0] M_DCACHE_AXI_AWADDR,
-	output [7:0] M_DCACHE_AXI_AWLEN,
-	output [2:0] M_DCACHE_AXI_AWSIZE,
-	output [1:0] M_DCACHE_AXI_AWBURST,
-	// output M_DCACHE_AXI_AWLOCK,
-	// output [3:0] M_DCACHE_AXI_AWCACHE,
-	// output [2:0] M_DCACHE_AXI_AWPROT,
-	// output [3:0] M_DCACHE_AXI_AWQOS,
-	output [7:0] M_DCACHE_AXI_AWUSER,
 	output M_DCACHE_AXI_AWVALID,
 	input M_DCACHE_AXI_AWREADY,
 
 	output [63:0] M_DCACHE_AXI_WDATA,
 	output [7:0] M_DCACHE_AXI_WSTRB,
-	output M_DCACHE_AXI_WLAST,
-	output [7:0] M_DCACHE_AXI_WUSER,
 	output M_DCACHE_AXI_WVALID,
 	input M_DCACHE_AXI_WREADY,
 
-	input [7:0] M_DCACHE_AXI_BID,
 	input [1:0] M_DCACHE_AXI_BRESP,
-	input [7:0] M_DCACHE_AXI_BUSER,
 	input M_DCACHE_AXI_BVALID,
 	output M_DCACHE_AXI_BREADY,
 
-	output [7:0] M_DCACHE_AXI_ARID,
 	output [63:0] M_DCACHE_AXI_ARADDR,
-	output [7:0] M_DCACHE_AXI_ARLEN,
-	output [2:0] M_DCACHE_AXI_ARSIZE,
-	output [1:0] M_DCACHE_AXI_ARBURST,
-	// output M_DCACHE_AXI_ARLOCK,
-	// output [3:0] M_DCACHE_AXI_ARCACHE,
-	// output [2:0] M_DCACHE_AXI_ARPROT,
-	// output [3:0] M_DCACHE_AXI_ARQOS,
-	output [7:0] M_DCACHE_AXI_ARUSER,
 	output M_DCACHE_AXI_ARVALID,
 	input M_DCACHE_AXI_ARREADY,
 
-	input [7:0] M_DCACHE_AXI_RID,
 	input [63 0] M_DCACHE_AXI_RDATA,
 	input [1:0] M_DCACHE_AXI_RRESP,
-	input M_DCACHE_AXI_RLAST,
-	input [7 0] M_DCACHE_AXI_RUSER,
 	input M_DCACHE_AXI_RVALID,
 	output M_DCACHE_AXI_RREADY
+
+	//ROM
+	output [63:0] M_ROM_AXI_ARADDR,
+	output M_ROM_AXI_ARVALID,
+	input M_ROM_AXI_ARREADY,
+
+	input [63 0] M_ROM_AXI_RDATA,
+	input [1:0] M_ROM_AXI_RRESP,
+	input M_ROM_AXI_RVALID,
+	output M_ROM_AXI_RREADY
+
+	//CLINT
+	output [63:0] M_CLINT_AXI_AWADDR,
+	output M_CLINT_AXI_AWVALID,
+	input M_CLINT_AXI_AWREADY,
+
+	output [63:0] M_CLINT_AXI_WDATA,
+	output [7:0] M_CLINT_AXI_WSTRB,
+	output M_CLINT_AXI_WVALID,
+	input M_CLINT_AXI_WREADY,
+
+	input [1:0] M_CLINT_AXI_BRESP,
+	input M_CLINT_AXI_BVALID,
+	output M_CLINT_AXI_BREADY,
+
+	output [63:0] M_CLINT_AXI_ARADDR,
+	output M_CLINT_AXI_ARVALID,
+	input M_CLINT_AXI_ARREADY,
+
+	input [63 0] M_CLINT_AXI_RDATA,
+	input [1:0] M_CLINT_AXI_RRESP,
+	input M_CLINT_AXI_RVALID,
+	output M_CLINT_AXI_RREADY
+
+	//PLIC
+	output [63:0] M_PLIC_AXI_AWADDR,
+	output M_PLIC_AXI_AWVALID,
+	input M_PLIC_AXI_AWREADY,
+
+	output [63:0] M_PLIC_AXI_WDATA,
+	output [7:0] M_PLIC_AXI_WSTRB,
+	output M_PLIC_AXI_WVALID,
+	input M_PLIC_AXI_WREADY,
+
+	input [1:0] M_PLIC_AXI_BRESP,
+	input M_PLIC_AXI_BVALID,
+	output M_PLIC_AXI_BREADY,
+
+	output [63:0] M_PLIC_AXI_ARADDR,
+	output M_PLIC_AXI_ARVALID,
+	input M_PLIC_AXI_ARREADY,
+
+	input [63 0] M_PLIC_AXI_RDATA,
+	input [1:0] M_PLIC_AXI_RRESP,
+	input M_PLIC_AXI_RVALID,
+	output M_PLIC_AXI_RREADY
+
+
+
+
+
+
+
 
 
 	input S_DM_AXI_ACLK,
 	input S_DM_AXI_ARESETN,
 	input S_FR_AXI_ACLK,
 	input S_FR_AXI_ARESETN,
-	// input S_IFU_AXI_ACLK,
-	// input S_IFU_AXI_ARESETN,
-	// input S_LSU_AXI_ACLK,
-	// input S_LSU_AXI_ARESETN,
-	// input M_PB_AXI_ACLK,
-	// input M_PB_AXI_ARESETN,
+
 	input M_SYS_AXI_ACLK,
 	input M_SYS_AXI_ARESETN,
-	// input M_ICACHE_AXI_ACLK,
-	// input M_ICACHE_AXI_ARESETN,
-	// input M_DCACHE_AXI_ACLK,
-	// input M_DCACHE_AXI_ARESETN,
+
 
 	input CLK,
 	input RSTn
@@ -378,6 +413,37 @@ module inner_crossBar (
 
 
 
+	//i fetch
+
+
+
+
+
+	wire isIFUSelIcache = ( |S_IFU_AXI_ARADDR[63:32]) & icache_enable;
+
+
+
+	assign S_IFU_AXI_RDATA = ({32{isIFUSelIcache}} & M_ICACHE_AXI_RDATA)
+							|
+
+
+	assign S_IFU_AXI_RRESP = ({2{isIFUSelIcache}} & M_ICACHE_AXI_RRESP)
+							|
+
+
+	output S_IFU_AXI_RVALID = (isIFUSelIcache & M_ICACHE_AXI_RVALID)
+							|
+
+
+	assign S_IFU_AXI_ARREADY = (isIFUSelIcache & M_ICACHE_AXI_ARREADY)
+								|
+
+
+
+	assign M_ICACHE_AXI_ARADDR = S_IFU_AXI_ARADDR;
+	assign M_ICACHE_AXI_ARVALID = isIFUSelIcache & S_IFU_AXI_ARVALID;
+	assign M_ICACHE_AXI_RREADY = (isIFUSelIcache & S_IFU_AXI_RREADY)
+								|
 
 
 
@@ -385,39 +451,76 @@ module inner_crossBar (
 
 
 
+	output [63:0] M_PB_AXI_AWADDR,
+	output M_PB_AXI_AWVALID,
+	input M_PB_AXI_AWREADY,
+
+	output [63:0] M_PB_AXI_WDATA,
+	output [7:0] M_PB_AXI_WSTRB,
+	output M_PB_AXI_WVALID,
+	input M_PB_AXI_WREADY,
+
+	input [1:0] M_PB_AXI_BRESP,
+	input M_PB_AXI_BVALID,
+	output M_PB_AXI_BREADY,
+
+	output [63:0] M_PB_AXI_ARADDR,
+	output M_PB_AXI_ARVALID,
+	input M_PB_AXI_ARREADY,
+
+	input [63 0] M_PB_AXI_RDATA,
+	input [1:0] M_PB_AXI_RRESP,
+	input M_PB_AXI_RVALID,
+	output M_PB_AXI_RREADY,
 
 
 
+	output [7:0] M_SYS_AXI_AWID,
+	output [63:0] M_SYS_AXI_AWADDR,
+	output [7:0] M_SYS_AXI_AWLEN,
+	output [2:0] M_SYS_AXI_AWSIZE,
+	output [1:0] M_SYS_AXI_AWBURST,
+	// output M_SYS_AXI_AWLOCK,
+	// output [3:0] M_SYS_AXI_AWCACHE,
+	// output [2:0] M_SYS_AXI_AWPROT,
+	// output [3:0] M_SYS_AXI_AWQOS,
+	output [7:0] M_SYS_AXI_AWUSER,
+	output M_SYS_AXI_AWVALID,
+	input M_SYS_AXI_AWREADY,
 
+	output [63:0] M_SYS_AXI_WDATA,
+	output [7:0] M_SYS_AXI_WSTRB,
+	output M_SYS_AXI_WLAST,
+	output [7:0] M_SYS_AXI_WUSER,
+	output M_SYS_AXI_WVALID,
+	input M_SYS_AXI_WREADY,
 
+	input [7:0] M_SYS_AXI_BID,
+	input [1:0] M_SYS_AXI_BRESP,
+	input [7:0] M_SYS_AXI_BUSER,
+	input M_SYS_AXI_BVALID,
+	output M_SYS_AXI_BREADY,
 
-	input [63:0] S_IFU_AXI_ARADDR,
-	input S_IFU_AXI_ARVALID,
-	output S_IFU_AXI_ARREADY,
+	output [7:0] M_SYS_AXI_ARID,
+	output [63:0] M_SYS_AXI_ARADDR,
+	output [7:0] M_SYS_AXI_ARLEN,
+	output [2:0] M_SYS_AXI_ARSIZE,
+	output [1:0] M_SYS_AXI_ARBURST,
+	// output M_SYS_AXI_ARLOCK,
+	// output [3:0] M_SYS_AXI_ARCACHE,
+	// output [2:0] M_SYS_AXI_ARPROT,
+	// output [3:0] M_SYS_AXI_ARQOS,
+	output [7:0] M_SYS_AXI_ARUSER,
+	output M_SYS_AXI_ARVALID,
+	input M_SYS_AXI_ARREADY,
 
-	output [63:0] S_IFU_AXI_RDATA,
-	output [1:0] S_IFU_AXI_RRESP,
-	output S_IFU_AXI_RVALID,
-	input S_IFU_AXI_RREADY,
-
-
-	input icache_enable,
-
-	output [63:0] M_ICACHE_AXI_ARADDR,
-	output M_ICACHE_AXI_ARVALID,
-	input M_ICACHE_AXI_ARREADY,
-
-	input [63 0] M_ICACHE_AXI_RDATA,
-	input [1:0] M_ICACHE_AXI_RRESP,
-	input M_ICACHE_AXI_RVALID,
-	output M_ICACHE_AXI_RREADY,
-
-
-
-
-
-
-
+	input [7:0] M_SYS_AXI_RID,
+	input [63 0] M_SYS_AXI_RDATA,
+	input [1:0] M_SYS_AXI_RRESP,
+	input M_SYS_AXI_RLAST,
+	input [7 0] M_SYS_AXI_RUSER,
+	input M_SYS_AXI_RVALID,
+	output M_SYS_AXI_RREADY,
 
 
 endmodule
