1929ecc9b072b431f8fa94674396dd85438d20c9
Ruige Lee <295054118@qq.com>
2020-12-22 19:32:04 +0800

    rv64m decoder

--- core/riftCore/backend/dispatch.v
+++ core/riftCore/backend/dispatch.v
@@ -4,7 +4,7 @@
 * @Email: wut.ruigeli@gmail.com
 * @Date:   2020-09-11 15:39:15
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2020-11-17 15:33:10
+* @Last Modified time: 2020-12-22 10:46:21
 */
 
 /*
@@ -76,75 +76,16 @@ module dispatch (
 
 	wire dispat_vaild = (~instrFifo_empty) & (~rd0_runOut) & (~reOrder_fifo_full);
 
-	wire rv64i_lui;
-	wire rv64i_auipc;
-
-	wire rv64i_jal;
-	wire rv64i_jalr;
-	wire rv64i_beq;
-	wire rv64i_bne;
-	wire rv64i_blt;
-	wire rv64i_bge;
-	wire rv64i_bltu;
-	wire rv64i_bgeu;
-
-	wire rv64i_lb;
-	wire rv64i_lh;
-	wire rv64i_lw;
-	wire rv64i_lbu;
-	wire rv64i_lhu;
-	wire rv64i_lwu;
-	wire rv64i_ld;
-
-	wire rv64i_sb;
-	wire rv64i_sh;
-	wire rv64i_sw;
-	wire rv64i_sd;
-
-	wire rv64i_addi;
-	wire rv64i_addiw;
-	wire rv64i_slti;
-	wire rv64i_sltiu;
-	wire rv64i_xori;
-	wire rv64i_ori;
-	wire rv64i_andi;
-	wire rv64i_slli;
-	wire rv64i_slliw;
-	wire rv64i_srli;
-	wire rv64i_srliw;
-	wire rv64i_srai;
-	wire rv64i_sraiw;
-
-	wire rv64i_add;
-	wire rv64i_addw;
-	wire rv64i_sub;
-	wire rv64i_subw;
-	wire rv64i_sll;
-	wire rv64i_sllw;
-	wire rv64i_slt;
-	wire rv64i_sltu;
-	wire rv64i_xor;
-	wire rv64i_srl;
-	wire rv64i_srlw;
-	wire rv64i_sra;
-	wire rv64i_sraw;
-	wire rv64i_or;
-	wire rv64i_and;
-
-	wire rv64i_fence;
-	wire rv64zi_fence_i;
-
-
-	wire rv64csr_rw;
-	wire rv64csr_rs;
-	wire rv64csr_rc;
-	wire rv64csr_rwi;
-	wire rv64csr_rsi;
-	wire rv64csr_rci;
-
-	wire privil_ecall;
-	wire privil_ebreak;
-	wire privil_mret;
+	wire rv64i_lui, rv64i_auipc;
+	wire rv64i_jal, rv64i_jalr, rv64i_beq, rv64i_bne, rv64i_blt, rv64i_bge, rv64i_bltu, rv64i_bgeu;
+	wire rv64i_lb, rv64i_lh, rv64i_lw, rv64i_lbu, rv64i_lhu, rv64i_lwu, rv64i_ld;
+	wire rv64i_sb, rv64i_sh, rv64i_sw, rv64i_sd;
+	wire rv64i_addi, rv64i_addiw, rv64i_slti, rv64i_sltiu, rv64i_xori, rv64i_ori, rv64i_andi, rv64i_slli, rv64i_slliw, rv64i_srli, rv64i_srliw, rv64i_srai, rv64i_sraiw;
+	wire rv64i_add, rv64i_addw, rv64i_sub, rv64i_subw, rv64i_sll, rv64i_sllw, rv64i_slt, rv64i_sltu, rv64i_xor, rv64i_srl, rv64i_srlw, rv64i_sra, rv64i_sraw, rv64i_or, rv64i_and;
+	wire rv64i_fence, rv64zi_fence_i;
+	wire rv64csr_rw, rv64csr_rs, rv64csr_rc, rv64csr_rwi, rv64csr_rsi, rv64csr_rci;
+	wire privil_ecall, privil_ebreak, privil_mret;
+	wire rv64m_mul, rv64m_mulh, rv64m_mullhsu, rv64m_mulhu, rv64m_div, rv64m_divu, rv64m_rem, rv64m_remu, rv64m_mulw, rv64m_divw, rv64m_divuw, rv64_remw, rv64m_remuw;
 
 	wire is_rvc;
 
@@ -164,6 +105,7 @@ module dispatch (
 				rv64i_fence, rv64zi_fence_i,
 				rv64csr_rw, rv64csr_rs, rv64csr_rc, rv64csr_rwi, rv64csr_rsi, rv64csr_rci,
 				privil_ecall, privil_ebreak, privil_mret,
+				rv64m_mul, rv64m_mulh, rv64m_mullhsu, rv64m_mulhu, rv64m_div, rv64m_divu, rv64m_rem, rv64m_remu, rv64m_mulw, rv64m_divw, rv64m_divuw, rv64_remw, rv64m_remuw,
 				is_rvc,
 				pc, imm, shamt, rd0_raw, rs1_raw, rs2_raw
 			} = decode_microInstr_pop;
