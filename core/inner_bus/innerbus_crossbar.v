8fc0cc89a243899501ece950a9dd65680a47a4e3
Ruige Lee <295054118@qq.com>
2021-01-03 12:18:10 +0800

    update licences

--- core/inner_bus/innerbus_crossbar.v
+++ core/inner_bus/innerbus_crossbar.v
@@ -4,11 +4,11 @@
 * @Email: wut.ruigeli@gmail.com
 * @Date:   2020-12-31 17:04:44
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2020-12-31 18:02:19
+* @Last Modified time: 2021-01-03 12:04:36
 */
 
 /*
-	Copyright (c) 2020 - 2020 Ruige Lee <wut.ruigeli@gmail.com>
+	Copyright (c) 2020 - 2021 Ruige Lee <wut.ruigeli@gmail.com>
 
 	 Licensed under the Apache License, Version 2.0 (the "License");
 	 you may not use this file except in compliance with the License.
