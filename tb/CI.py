10d0f7351cae5e9bf100905415b41f0159aa4e2c
Ruige Lee <295054118@qq.com>
2021-01-19 15:47:33 +0800

    update axi xbar

--- tb/CI.py
+++ tb/CI.py
@@ -2,7 +2,7 @@
 # @Author: Ruige Lee
 # @Date:   2020-11-18 15:37:18
 # @Last Modified by:   Ruige Lee
-# @Last Modified time: 2021-01-15 19:46:54
+# @Last Modified time: 2021-01-19 15:44:56
 
 
 import sys
@@ -90,7 +90,7 @@ testList = [
 
 
 
-res = os.system("iverilog.exe -W all -o ./build/wave.iverilog  -y ../RiftChip/ -y ../RiftChip/riftCore/ -y ../RiftChip/riftCore/backend  -y ../RiftChip/riftCore/backend/issue  -y ../RiftChip/riftCore/backend/execute  -y ../RiftChip/riftCore/frontend  -y ../RiftChip/element -y ../RiftChip/Soc -y ../RiftChip/debug -y ../tb  -I ../tb  -I ../RiftChip/  ../tb/riftChip_CI.v  ")
+res = os.system("iverilog.exe -W all -o ./build/wave.iverilog  -y ../RiftChip/ -y ../RiftChip/riftCore/ -y ../RiftChip/riftCore/backend  -y ../RiftChip/riftCore/backend/issue  -y ../RiftChip/riftCore/backend/execute  -y ../RiftChip/riftCore/frontend  -y ../RiftChip/element -y ../RiftChip/Soc -y ../RiftChip/Soc/xilinx_IP/axi_Xbar -y ../RiftChip/debug -y ../tb  -I ../tb  -I ../RiftChip/  ../tb/riftChip_CI.v  ")
 
 if ( res == 0 ):
 	print ("compile pass!")
