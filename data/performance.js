b926f6bb57090ca2094d5cc59447b8af3b703075
Ruige Lee <295054118@qq.com>
2021-02-07 20:00:26 +0800

    update

--- data/performance.js
+++ data/performance.js
@@ -1,5 +1,5 @@
 data = 
 {
   'lastUpdata': 1612688520,
-  'benchmark': [{'hash': '0e65a43', 'author name': 'Ruige Lee', 'author email': '295054118@qq.com', 'author date': ' Fri Jan  1 12:13:35 2021 ', 'commit comment': '0.4.2', 'dhrystone': 0.568016}, {'hash': '7feda7e', 'author name': 'Ruige Lee', 'author email': '295054118@qq.com', 'author date': 'Sun Feb 7 17:00:51 2021 +0800', 'commit comment': 'update ds script', 'dhrystone': 0.170517}, {'hash': '7feda7e', 'author name': 'Ruige Lee', 'author email': '295054118@qq.com', 'author date': 'Sun Feb 7 17:00:51 2021 +0800', 'commit comment': 'update ds script', 'dhrystone': 0.170517}, {'hash': '7feda7e', 'author name': 'Ruige Lee', 'author email': '295054118@qq.com', 'author date': 'Sun Feb 7 17:00:51 2021 +0800', 'commit comment': 'update ds script', 'dhrystone': 0.170517}]
+  'benchmark': [{'hash': '0e65a43', 'author name': 'Ruige Lee', 'author email': '295054118@qq.com', 'author date': ' Fri Jan  1 12:13:35 2021 ', 'commit comment': '0.4.2', 'dhrystone': 0.568016}, {'hash': '7feda7e', 'author name': 'Ruige Lee', 'author email': '295054118@qq.com', 'author date': 'Sun Feb 7 17:00:51 2021 +0800', 'commit comment': 'update ds script', 'dhrystone': 0.170517}]
 }
