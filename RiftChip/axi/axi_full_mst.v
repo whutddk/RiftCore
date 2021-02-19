4c2981c7d57128acde728d41abbff373b1a96b3e
Ruige Lee <295054118@qq.com>
2021-02-19 17:38:36 +0800

    axi full complete

--- RiftChip/axi/axi_full_mst.v
+++ RiftChip/axi/axi_full_mst.v
@@ -4,7 +4,7 @@
 * @Email: wut.ruigeli@gmail.com
 * @Date:   2021-02-19 11:37:20
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2021-02-19 15:27:32
+* @Last Modified time: 2021-02-19 17:26:59
 */
 
 
@@ -43,13 +43,7 @@ module axi_full_mst #
 )
 (
 
-	// input wire  INIT_AXI_TXN,
-	// output wire  TXN_DONE,
-	// output reg  ERROR,
-
-
-
-	output [ID_W-1 : 0] M_AXI_AWID,
+	output [ID_W-1:0] M_AXI_AWID,
 	output [AW-1:0] M_AXI_AWADDR,
 	output [7:0] M_AXI_AWLEN,
 	output [2:0] M_AXI_AWSIZE,
@@ -63,34 +57,34 @@ module axi_full_mst #
 	input M_AXI_AWREADY,
 
 	output [DW-1:0] M_AXI_WDATA,
-	output [DW/8-1 : 0] M_AXI_WSTRB,
+	output [DW/8-1:0] M_AXI_WSTRB,
 	output M_AXI_WLAST,
-	output [USER_W-1 : 0] M_AXI_WUSER,
+	output [USER_W-1:0] M_AXI_WUSER,
 	output M_AXI_WVALID,
 	input M_AXI_WREADY,
 
-	input [ID_W-1 : 0] M_AXI_BID,
-	input [1 : 0] M_AXI_BRESP,
-	input [USER_W-1 : 0] M_AXI_BUSER,
+	input [ID_W-1:0] M_AXI_BID,
+	input [1:0] M_AXI_BRESP,
+	input [USER_W-1:0] M_AXI_BUSER,
 	input M_AXI_BVALID,
 	output M_AXI_BREADY,
 
-	output [ID_W-1 : 0] M_AXI_ARID,
-	output [AW-1 : 0] M_AXI_ARADDR,
-	output [7 : 0] M_AXI_ARLEN,
-	output [2 : 0] M_AXI_ARSIZE,
-	output [1 : 0] M_AXI_ARBURST,
-	output  M_AXI_ARLOCK,
-	output [3 : 0] M_AXI_ARCACHE,
-	output [2 : 0] M_AXI_ARPROT,
-	output [3 : 0] M_AXI_ARQOS,
-	output [CUSER_W-1 : 0] M_AXI_ARUSER,
+	output [ID_W-1:0] M_AXI_ARID,
+	output [AW-1:0] M_AXI_ARADDR,
+	output [7:0] M_AXI_ARLEN,
+	output [2:0] M_AXI_ARSIZE,
+	output [1:0] M_AXI_ARBURST,
+	output M_AXI_ARLOCK,
+	output [3:0] M_AXI_ARCACHE,
+	output [2:0] M_AXI_ARPROT,
+	output [3:0] M_AXI_ARQOS,
+	output [USER_W-1:0] M_AXI_ARUSER,
 	output M_AXI_ARVALID,
 	input M_AXI_ARREADY,
 
-	input [ID_W-1 : 0] M_AXI_RID,
-	input [DW-1 : 0] M_AXI_RDATA,
-	input [1 : 0] M_AXI_RRESP,
+	input [ID_W-1:0] M_AXI_RID,
+	input [DW-1:0] M_AXI_RDATA,
+	input [1:0] M_AXI_RRESP,
 	input M_AXI_RLAST,
 	input [USER_W-1:0] M_AXI_RUSER,
 	input M_AXI_RVALID,
@@ -101,74 +95,86 @@ module axi_full_mst #
 );
 
 
+	wire axi_awvalid_set, axi_awvalid_rst, axi_awvalid_qout;
+	wire axi_wvalid_set, axi_wvalid_rst, axi_wvalid_qout;
+	wire axi_wlast_set, axi_wlast_rst, axi_wlast_qout;                                                                                    
+	wire [7:0] write_index_dnxt;
+	wire [7:0] write_index_qout;
+	wire axi_bready_set, axi_bready_rst, axi_bready_qout;
+	wire axi_arvalid_set, axi_arvalid_rst, axi_arvalid_qout;
+	wire [7:0] read_index_dnxt;
+	wire [7:0] read_index_qout;
+	wire axi_rready_set, axi_rready_rst, axi_rready_qout;
+	wire wnext, rnext;
+	wire write_resp_error, read_resp_error;
+	wire start_single_burst_read, start_single_burst_write;
 
-	assign M_AXI_AWID	= 'b0;
+
+	assign M_AXI_AWID = 'b0;
 	assign M_AXI_AWADDR	= ;
-	assign M_AXI_AWLEN	= C_M_AXI_BURST_LEN - 1;//Burst LENgth is number of transaction beats, minus 1
-	assign M_AXI_AWSIZE	= clogb2((C_M_AXI_DATA_WIDTH/8)-1);//Size should be C_M_AXI_DATA_WIDTH, in 2^SIZE bytes, otherwise narrow bursts are used
+	assign M_AXI_AWLEN = C_M_AXI_BURST_LEN - 1;//Burst LENgth is number of transaction beats, minus 1
+	assign M_AXI_AWSIZE	= $clog2(DW/8);//Size should be C_M_AXI_DATA_WIDTH, in 2^SIZE bytes, otherwise narrow bursts are used
 	assign M_AXI_AWBURST = 2'b01;//INCR burst type is usually used, except for keyhole bursts
 	assign M_AXI_AWLOCK	= 1'b0;
-	assign M_AXI_AWCACHE	= 4'b0010;//Update value to 4'b0011 if coherent accesses to be used via the Zynq ACP port. Not Allocated, Modifiable, not Bufferable. Not Bufferable since this example is meant to test memory, not intermediate cache. 
+	assign M_AXI_AWCACHE = 4'b0010;//Update value to 4'b0011 if coherent accesses to be used via the Zynq ACP port. Not Allocated, Modifiable, not Bufferable. Not Bufferable since this example is meant to test memory, not intermediate cache. 
 	assign M_AXI_AWPROT	= 3'h0;
-	assign M_AXI_AWQOS	= 4'h0;
+	assign M_AXI_AWQOS = 4'h0;
 	assign M_AXI_AWUSER	= 'b1;
-	assign M_AXI_AWVALID	= axi_awvalid;
+	assign M_AXI_AWVALID = axi_awvalid_qout;
 
-	assign M_AXI_WDATA	= axi_wdata;
-	assign M_AXI_WSTRB	= {(DW/8){1'b1}};//All bursts are complete and aligned in this example
-	assign M_AXI_WLAST	= axi_wlast;
-	assign M_AXI_WUSER	= 'b0;
-	assign M_AXI_WVALID	= axi_wvalid;
+	assign M_AXI_WDATA = ;
+	assign M_AXI_WSTRB = {(DW/8){1'b1}};//All bursts are complete and aligned in this example
+	assign M_AXI_WLAST = axi_wlast_qout;
+	assign M_AXI_WUSER = 'b0;
+	assign M_AXI_WVALID	= axi_wvalid_qout;
 
-	assign M_AXI_BREADY	= axi_bready;
+	assign M_AXI_BREADY	= axi_bready_qout;
 
 
-	assign M_AXI_ARID	= 'b0;
+	assign M_AXI_ARID = 'b0;
 	assign M_AXI_ARADDR	= ;
 	
 	assign M_AXI_ARLEN	= C_M_AXI_BURST_LEN - 1;//Burst LENgth is number of transaction beats, minus 1
-	assign M_AXI_ARSIZE	= clogb2((C_M_AXI_DATA_WIDTH/8)-1);//Size should be C_M_AXI_DATA_WIDTH, in 2^n bytes, otherwise narrow bursts are used
-	assign M_AXI_ARBURST	= 2'b01;//INCR burst type is usually used, except for keyhole bursts
+	assign M_AXI_ARSIZE	= $clog2(DW/8);//Size should be C_M_AXI_DATA_WIDTH, in 2^n bytes, otherwise narrow bursts are used
+	assign M_AXI_ARBURST = 2'b01;//INCR burst type is usually used, except for keyhole bursts
 	assign M_AXI_ARLOCK	= 1'b0;
 	
-	assign M_AXI_ARCACHE	= 4'b0010;//Update value to 4'b0011 if coherent accesses to be used via the Zynq ACP port. Not Allocated, Modifiable, not Bufferable. Not Bufferable since this example is meant to test memory, not intermediate cache. 
+	assign M_AXI_ARCACHE = 4'b0010;//Update value to 4'b0011 if coherent accesses to be used via the Zynq ACP port. Not Allocated, Modifiable, not Bufferable. Not Bufferable since this example is meant to test memory, not intermediate cache. 
 	assign M_AXI_ARPROT	= 3'h0;
 	assign M_AXI_ARQOS	= 4'h0;
 	assign M_AXI_ARUSER	= 'b1;
-	assign M_AXI_ARVALID	= axi_arvalid;
+	assign M_AXI_ARVALID = axi_arvalid_qout;
 	
-	assign M_AXI_RREADY	= axi_rready;
+	assign M_AXI_RREADY	= axi_rready_qout;
 
                                                        
 	                                                                       
-	wire axi_awvalid_set, axi_awvalid_rst, axi_awvalid_qout;
+
+
+
+
 	assign axi_awvalid_set = ~axi_awvalid_qout & start_single_burst_write;
 	assign axi_awvalid_rst =  axi_awvalid_qout & M_AXI_AWREADY ;
 	gen_rsffr axi_awvalid_rsffr (.set_in(axi_awvalid_set), .rst_in(axi_awvalid_rst), .qout(axi_awvalid_qout), .CLK(CLK), .RSTn(RSTn));
 
 
 
-
-
-
 	assign wnext = M_AXI_WREADY & axi_wvalid_qout;
 
-	wire axi_wvalid_set, axi_wvalid_rst, axi_wvalid_qout;
+
 	assign axi_wvalid_set = (~axi_wvalid_qout & start_single_burst_write);
 	assign axi_wvalid_rst = (wnext & axi_wlast_qout) ;
 	gen_rsffr axi_wvalid_rsffr (.set_in(axi_wvalid_set), .rst_in(axi_wvalid_rst), .qout(axi_wvalid_qout), .CLK(CLK), .RSTn(RSTn));
 
 
 
-	wire axi_wlast_set, axi_wlast_rst, axi_wlast_qout;
-	assign axi_wlast_set = ((write_index == C_M_AXI_BURST_LEN-2 && C_M_AXI_BURST_LEN >= 2) && wnext) || (C_M_AXI_BURST_LEN == 1 );
+
+	assign axi_wlast_set = ((write_index_qout == C_M_AXI_BURST_LEN-2 && C_M_AXI_BURST_LEN >= 2) && wnext) || (C_M_AXI_BURST_LEN == 1 );
 	assign axi_wlast_rst = ~axi_wlast_set & ( wnext | (axi_wlast_qout && C_M_AXI_BURST_LEN == 1) );
 	gen_rsffr axi_wlast_rsffr (.set_in(axi_wlast_set), .rst_in(axi_wlast_rst), .qout(axi_wlast_qout), .CLK(CLK), .RSTn(RSTn));
 
                                                                                                 
-	                                                                                    
-	wire [7:0] write_index_dnxt;
-	wire [7:0] write_index_qout;
+
 	assign write_index_dnxt = start_single_burst_write ? 8'd0 :
 								(
 									(wnext && (write_index_qout != C_M_AXI_BURST_LEN-1)) ? (write_index_qout + 8'd1) : write_index_qout
@@ -176,27 +182,25 @@ module axi_full_mst #
 	gen_dffr # (.DW(8)) write_index_dffr (.dnxt(write_index_dnxt), .qout(write_index_qout), .CLK(CLK), .RSTn(RSTn));
 
 
-	wire axi_bready_set, axi_bready_rst, axi_bready_qout;
 	assign axi_bready_set = (M_AXI_BVALID && ~axi_bready_qout);
 	assign axi_bready_rst = axi_bready_qout;
 	gen_rsffr axi_bready_rsffr (.set_in(axi_bready_set), .rst_in(axi_bready_rst), .qout(axi_bready_qout), .CLK(CLK), .RSTn(RSTn));
 	
 
-	assign write_resp_error = axi_bready & M_AXI_BVALID & M_AXI_BRESP[1]; 
+	assign write_resp_error = axi_bready_qout & M_AXI_BVALID & M_AXI_BRESP[1]; 
 
 
 
 
-	wire axi_arvalid_set, axi_arvalid_rst, axi_arvalid_qout;
-	assign axi_arvalid_set = ~axi_arvalid & start_single_burst_read;
-	assign axi_arvalid_rst = axi_arvalid & M_AXI_ARREADY ;
+	assign axi_arvalid_set = ~axi_arvalid_qout & start_single_burst_read;
+	assign axi_arvalid_rst = axi_arvalid_qout & M_AXI_ARREADY ;
 	gen_rsffr axi_arvalid_rsffr (.set_in(axi_arvalid_set), .rst_in(axi_arvalid_rst), .qout(axi_arvalid_qout), .CLK(CLK), .RSTn(RSTn));
 	
 
-	assign rnext = M_AXI_RVALID && axi_rready;                            
+	assign rnext = M_AXI_RVALID && axi_rready_qout;                            
 
-	wire [7:0] read_index_dnxt;
-	wire [7:0] read_index_qout;
+
+
 	assign read_index_dnxt = start_single_burst_read ? 8'd0 :
 								(
 									(rnext & (read_index != C_M_AXI_BURST_LEN-1)) ? (read_index_qout + 8'd1) : read_index_qout
@@ -204,7 +208,6 @@ module axi_full_mst #
 	gen_dffr # (.DW(8)) read_index_dffr (.dnxt(read_index_dnxt), .qout(read_index_qout), .CLK(CLK), .RSTn(RSTn));
 
 
-	wire axi_rready_set, axi_rready_rst, axi_rready_qout;
 	assign axi_rready_set = M_AXI_RVALID & (~M_AXI_RLAST | ~axi_rready_qout);
 	assign axi_rready_rst = M_AXI_RVALID &   M_AXI_RLAST &  axi_rready_qout;
 	gen_rsffr axi_rready_rsffr (.set_in(axi_rready_set), .rst_in(axi_rready_rst), .qout(axi_rready_qout), .CLK(CLK), .RSTn(RSTn));
--- RiftChip/axi/axi_full_slv.v
+++ RiftChip/axi/axi_full_slv.v
@@ -4,7 +4,7 @@
 * @Email: wut.ruigeli@gmail.com
 * @Date:   2021-02-19 14:57:31
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2021-02-19 16:54:47
+* @Last Modified time: 2021-02-19 17:15:34
 */
 
 
@@ -93,43 +93,77 @@ module axi_full_slv
 
 	localparam ADDR_LSB = $clog2(DW/8);
 
-	assign S_AXI_AWREADY	= axi_awready;
-	assign S_AXI_WREADY	= axi_wready;
-	assign S_AXI_BRESP	= 2'b00;
-	assign S_AXI_BUSER	= 'b0;
-	assign S_AXI_BVALID	= axi_bvalid;
-	assign S_AXI_ARREADY	= axi_arready;
-	assign S_AXI_RDATA	= axi_rdata;
-	assign S_AXI_RRESP	= 2'b00;
-	assign S_AXI_RLAST	= axi_rlast;
-	assign S_AXI_RUSER	= 'd0;
-	assign S_AXI_RVALID	= axi_rvalid;
-	assign S_AXI_BID = S_AXI_AWID;
-	assign S_AXI_RID = S_AXI_ARID;
-	assign  aw_wrap_size = (C_S_AXI_DATA_WIDTH/8 * (axi_awlen)); 
-	assign  ar_wrap_size = (C_S_AXI_DATA_WIDTH/8 * (axi_arlen)); 
-	assign  aw_wrap_en = ((axi_awaddr & aw_wrap_size) == aw_wrap_size)? 1'b1: 1'b0;
-	assign  ar_wrap_en = ((axi_araddr & ar_wrap_size) == ar_wrap_size)? 1'b1: 1'b0;
 
-
-
+	wire [AW-1:0] aw_wrap_size; 
+	wire [AW-1:0] ar_wrap_size; 
+	wire aw_wrap_en, ar_wrap_en;
 
 	wire axi_awready_set, axi_awready_rst, axi_awready_qout;
-	assign axi_awready_set =  (~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout);
-	assign axi_awready_rst = ~(~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout) & ~(S_AXI_WLAST & axi_wready);
-	gen_rsffr axi_awready_rsffr (.set_in(axi_awready_set), .rst_in(axi_awready_rst), .qout(axi_awready_qout), .CLK(CLK), .RSTn(RSTn));
-
 	wire axi_awv_awr_flag_set, axi_awv_awr_flag_rst, axi_awv_awr_flag_qout;
-	assign axi_awv_awr_flag_set = (~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout);
-	assign axi_awv_awr_flag_rst = ( axi_awready_qout | ~S_AXI_AWVALID | axi_awv_awr_flag_qout |  axi_arv_arr_flag_qout) & (S_AXI_WLAST & axi_wready_qout);
-	gen_rsffr axi_awv_awr_flag_rsffr (.set_in(axi_awv_awr_flag_set), .rst_in(axi_awv_awr_flag_rst), .qout(axi_awv_awr_flag_qout), .CLK(CLK), .RSTn(RSTn));
-
-
 	wire [AW-1:0] axi_awaddr_dnxta;
 	wire [AW-1:0] axi_awaddr_dnxtb;
 	wire [AW-1:0] axi_awaddr_qout;
-	wire axi_awaddr_ena;
-	wire axi_awaddr_enb;
+	wire axi_awaddr_ena, axi_awaddr_enb;
+	wire axi_awburst_en;
+	wire [1:0] axi_awburst_dnxt;
+	wire [1:0] axi_awburst_qout;
+	wire axi_awlen_en;
+	wire [7:0] axi_awlen_dnxt;
+	wire [7:0] axi_awlen_qout;
+	wire [7:0] axi_awlen_cnt_dnxta;
+	wire [7:0] axi_awlen_cnt_dnxtb;
+	wire [7:0] axi_awlen_cnt_qout;
+	wire axi_awlen_cnt_ena, axi_awlen_cnt_enb;
+	wire axi_wready_set, axi_wready_rst, axi_wready_qout;
+	wire axi_bvalid_set, axi_bvalid_rst, axi_bvalid_qout;
+	wire axi_arready_set, axi_arready_rst, axi_arready_qout;
+	wire axi_arv_arr_flag_set, axi_arv_arr_flag_rst, axi_arv_arr_flag_qout;
+	wire [AW-1:0] axi_araddr_dnxta;
+	wire [AW-1:0] axi_araddr_dnxtb;
+	wire [AW-1:0] axi_araddr_qout;
+	wire axi_araddr_ena, axi_araddr_enb, axi_arburst_en;
+	wire [1:0] axi_arburst_dnxt;
+	wire [1:0] axi_arburst_qout;
+	wire axi_arlen_en;
+	wire [7:0] axi_arlen_dnxt;
+	wire [7:0] axi_arlen_qout;
+	wire [7:0] axi_arlen_cnt_dnxta;
+	wire [7:0] axi_arlen_cnt_dnxtb;
+	wire [7:0] axi_arlen_cnt_qout;
+	wire axi_arlen_cnt_ena, axi_arlen_cnt_enb;
+	wire axi_rvalid_set, axi_rvalid_rst, axi_rvalid_qout;
+	wire axi_rlast_set, axi_rlast_rst, axi_rlast_qout;
+
+
+	assign S_AXI_AWREADY = axi_awready_qout;
+	assign S_AXI_WREADY	= axi_wready_qout;
+	assign S_AXI_BRESP = 2'b00;
+	assign S_AXI_BUSER = 'b0;
+	assign S_AXI_BVALID	= axi_bvalid_qout;
+	assign S_AXI_ARREADY = axi_arready_qout;
+	assign S_AXI_RDATA = ;
+	assign S_AXI_RRESP = 2'b00;
+	assign S_AXI_RLAST = axi_rlast_qout;
+	assign S_AXI_RUSER = 'd0;
+	assign S_AXI_RVALID	= axi_rvalid_qout;
+	assign S_AXI_BID = S_AXI_AWID;
+	assign S_AXI_RID = S_AXI_ARID;
+	assign aw_wrap_size = (C_S_AXI_DATA_WIDTH/8 * (axi_awlen_qout)); 
+	assign ar_wrap_size = (C_S_AXI_DATA_WIDTH/8 * (axi_arlen_qout)); 
+	assign aw_wrap_en = ((axi_awaddr_qout & aw_wrap_size) == aw_wrap_size) ? 1'b1 : 1'b0;
+	assign ar_wrap_en = ((axi_araddr_qout & ar_wrap_size) == ar_wrap_size) ? 1'b1 : 1'b0;
+
+
+
+
+
+	assign axi_awready_set =  (~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout);
+	assign axi_awready_rst = ~(~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout) & ~(S_AXI_WLAST & axi_wready_qout);
+	gen_rsffr axi_awready_rsffr (.set_in(axi_awready_set), .rst_in(axi_awready_rst), .qout(axi_awready_qout), .CLK(CLK), .RSTn(RSTn));
+
+	assign axi_awv_awr_flag_set = (~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout);
+	assign axi_awv_awr_flag_rst = ( axi_awready_qout | ~S_AXI_AWVALID | axi_awv_awr_flag_qout |  axi_arv_arr_flag_qout) & (S_AXI_WLAST & axi_wready_qout);
+	gen_rsffr axi_awv_awr_flag_rsffr (.set_in(axi_awv_awr_flag_set), .rst_in(axi_awv_awr_flag_rst), .qout(axi_awv_awr_flag_qout), .CLK(CLK), .RSTn(RSTn));
 
 	assign axi_awaddr_dnxta = S_AXI_AWADDR;
 	assign axi_awaddr_dnxtb = ( {AW{axi_awburst == 2'b00}} & axi_awaddr_qout )
@@ -147,55 +181,31 @@ module axi_full_slv
 	assign axi_awaddr_enb = (axi_awlen_cnt_qout <= axi_awlen_qout) & axi_wready_qout & S_AXI_WVALID;
 	gen_dpdffren # (.DW(AW)) axi_awaddr_dpdffren( .dnxta(axi_awaddr_dnxta), .ena(axi_awaddr_ena), .dnxtb(axi_awaddr_dnxtb), .enb(axi_awaddr_enb), .qout(axi_awaddr_qout), .CLK(CLK), .RSTn(RSTn) );
 
-
-	wire axi_awburst_en;
-	wire [1:0] axi_awburst_dnxt;
-	wire [1:0] axi_awburst_qout;
-
 	assign axi_awburst_en = (~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout);
 	assign axi_awburst_dnxt = S_AXI_AWBURST;
 	gen_dffren # (.DW(2)) axi_awburst_dffren (.dnxt(axi_awburst_dnxt), .qout(axi_awburst_qout), .en(axi_awburst_en), .CLK(CLK), .RSTn(RSTn));
 
 
-	wire axi_awlen_en;
-	wire [7:0] axi_awlen_dnxt;
-	wire [7:0] axi_awlen_qout;
+
 
 	assign axi_awlen_en = ~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout;
 	assign axi_awlen_dnxt = S_AXI_AWLEN;
 	gen_dffren # (.DW(8)) axi_awlen_dffren (.dnxt(axi_awlen_dnxt), .qout(axi_awlen_qout), .en(axi_awlen_en), .CLK(CLK), .RSTn(RSTn));
 
 
-
-
-
-	wire [7:0] axi_awlen_cnt_dnxta;
-	wire [7:0] axi_awlen_cnt_dnxtb;
-	wire [7:0] axi_awlen_cnt_qout;
-	wire axi_awlen_cnt_ena;
-	wire axi_awlen_cnt_enb;
-
-
 	assign axi_awlen_cnt_dnxta = 8'd0;
 	assign axi_awlen_cnt_dnxtb = axi_awlen_cnt_qout + 8'd1;
 	assign axi_awlen_cnt_ena = ~axi_awready_qout & S_AXI_AWVALID & ~axi_awv_awr_flag_qout;
-	assign axi_awlen_cnt_enb = (axi_awlen_cnt_qout <= axi_awlen) & axi_wready_qout & S_AXI_WVALID;
+	assign axi_awlen_cnt_enb = (axi_awlen_cnt_qout <= axi_awlen_qout) & axi_wready_qout & S_AXI_WVALID;
 	gen_dpdffren # (.DW(8)) axi_awlen_cnt_dpdffren( .dnxta(axi_awlen_cnt_dnxta), .ena(axi_awlen_cnt_ena), .dnxtb(axi_awlen_cnt_dnxtb), .enb(axi_awlen_cnt_enb), .qout(axi_awlen_cnt_qout), .CLK(CLK), .RSTn(RSTn) );
 
 
-
-
-
-
-	wire axi_wready_set, axi_wready_rst, axi_wready_qout;
-	assign axi_wready_set = ~axi_wready & S_AXI_WVALID & axi_awv_awr_flag_qout;
-	assign axi_wready_rst =  axi_wready & S_AXI_WLAST;
+	assign axi_wready_set = ~axi_wready_qout & S_AXI_WVALID & axi_awv_awr_flag_qout;
+	assign axi_wready_rst =  axi_wready_qout & S_AXI_WLAST;
 	gen_rsffr axi_wready_rsffr (.set_in(axi_wready_set), .rst_in(axi_wready_rst), .qout(axi_wready_qout), .CLK(CLK), .RSTn(RSTn));
 
 
 
-
-	wire axi_bvalid_set, axi_bvalid_rst, axi_bvalid_qout;
 	assign axi_bvalid_set = ~axi_bvalid_qout & axi_awv_awr_flag_qout & axi_wready_qout & S_AXI_WVALID & S_AXI_WLAST;
 	assign axi_bvalid_rst =  axi_bvalid_qout & S_AXI_BREADY;
 	gen_rsffr axi_bvalid_rsffr (.set_in(axi_bvalid_set), .rst_in(axi_bvalid_rst), .qout(axi_bvalid_qout), .CLK(CLK), .RSTn(RSTn));
@@ -203,86 +213,58 @@ module axi_full_slv
 
 
 
-
-
-
-
-
-	wire axi_arready_set, axi_arready_rst, axi_arready_qout;
-	assign axi_arready_set = (~axi_arready & S_AXI_ARVALID & ~axi_awv_awr_flag & ~axi_arv_arr_flag);
-	assign axi_arready_rst = ~(~axi_arready & S_AXI_ARVALID & ~axi_awv_awr_flag & ~axi_arv_arr_flag) & ~(axi_rvalid & S_AXI_RREADY & axi_arlen_cntr == axi_arlen);
+	
+	assign axi_arready_set = (~axi_arready & S_AXI_ARVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout);
+	assign axi_arready_rst = ~(~axi_arready & S_AXI_ARVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout) & ~(axi_rvalid_qout & S_AXI_RREADY & axi_arlen_cnt_qout == axi_arlen_qout);
 	gen_rsffr axi_arready_rsffr (.set_in(axi_arready_set), .rst_in(axi_arready_rst), .qout(axi_arready_qout), .CLK(CLK), .RSTn(RSTn));
 
-	wire axi_arv_arr_flag_set, axi_arv_arr_flag_rst, axi_arv_arr_flag_qout;
-	assign axi_arv_arr_flag_set = (~axi_arready & S_AXI_ARVALID & ~axi_awv_awr_flag & ~axi_arv_arr_flag);
-	assign axi_arv_arr_flag_rst = (axi_arready | ~S_AXI_ARVALID | axi_awv_awr_flag | axi_arv_arr_flag) & (axi_rvalid & S_AXI_RREADY & axi_arlen_cntr == axi_arlen);
+	assign axi_arv_arr_flag_set = (~axi_arready_qout &  S_AXI_ARVALID & ~axi_awv_awr_flag_qout & ~axi_arv_arr_flag_qout);
+	assign axi_arv_arr_flag_rst = ( axi_arready_qout | ~S_AXI_ARVALID |  axi_awv_awr_flag_qout |  axi_arv_arr_flag_qout) & (axi_rvalid_qout & S_AXI_RREADY & axi_arlen_cnt_qout == axi_arlen_qout);
 	gen_rsffr axi_arv_arr_flag_rsffr (.set_in(axi_arv_arr_flag_set), .rst_in(axi_arv_arr_flag_rst), .qout(axi_arv_arr_flag_qout), .CLK(CLK), .RSTn(RSTn));
 
 
 
-
-
-
-	wire [AW-1:0] axi_araddr_dnxta;
-	wire [AW-1:0] axi_araddr_dnxtb;
-	wire [AW-1:0] axi_araddr_qout;
-	wire axi_araddr_ena;
-	wire axi_araddr_enb;
-
 	assign axi_araddr_dnxta = S_AXI_ARADDR;
-	assign axi_araddr_dnxtb = ({AW{axi_arburst == 2'b00}} & axi_araddr_qout)
+	assign axi_araddr_dnxtb = ({AW{axi_arburst_qout == 2'b00}} & axi_araddr_qout)
 							|
-							({AW{axi_arburst == 2'b01}} & axi_araddr_qout + (1<<ADDR_LSB))
+							({AW{axi_arburst_qout == 2'b01}} & axi_araddr_qout + (1<<ADDR_LSB))
 							|
-							({AW{axi_arburst == 2'b10}} & 
+							({AW{axi_arburst_qout == 2'b10}} & 
 								(
 									({AW{ ar_wrap_en}} & (axi_araddr_qout - ar_wrap_size) )
 									|
 									({AW{~ar_wrap_en}} & (axi_araddr_qout + (1<<ADDR_LSB)))
 								)
 							);
-	assign axi_araddr_ena = (~axi_arready && S_AXI_ARVALID && ~axi_arv_arr_flag);
-	assign axi_araddr_enb = ((axi_arlen_cntr <= axi_arlen) && axi_rvalid && S_AXI_RREADY);
+	assign axi_araddr_ena = (~axi_arready_qout & S_AXI_ARVALID & ~axi_arv_arr_flag_qout);
+	assign axi_araddr_enb = ((axi_arlen_cnt_qout <= axi_arlen_qout) & axi_rvalid_qout & S_AXI_RREADY);
 	gen_dpdffren # (.DW(AW)) axi_araddr_dpdffren( .dnxta(axi_araddr_dnxta), .ena(axi_araddr_ena), .dnxtb(axi_araddr_dnxtb), .enb(axi_araddr_enb), .qout(axi_araddr_qout), .CLK(CLK), .RSTn(RSTn) );
 
-
-	wire axi_arburst_en;
-	wire [1:0] axi_arburst_dnxt;
-	wire [1:0] axi_arburst_qout;
-	assign axi_arburst_en = (~axi_arready && S_AXI_ARVALID && ~axi_arv_arr_flag);
+	
+	assign axi_arburst_en = (~axi_arready_qout & S_AXI_ARVALID & ~axi_arv_arr_flag_qout);
 	assign axi_arburst_dnxt = S_AXI_ARBURST;
 	gen_dffren # (.DW(2)) axi_arburst_dffren (.dnxt(axi_arburst_dnxt), .qout(axi_arburst_qout), .en(axi_arburst_en), .CLK(CLK), .RSTn(RSTn));
 
 
-	wire axi_arlen_en;
-	wire [7:0] axi_arlen_dnxt;
-	wire [7:0] axi_arlen_qout;
-	assign axi_arlen_en = (~axi_arready && S_AXI_ARVALID && ~axi_arv_arr_flag);
+	assign axi_arlen_en = (~axi_arready_qout && S_AXI_ARVALID && ~axi_arv_arr_flag_qout);
 	assign axi_arlen_dnxt = S_AXI_ARLEN;
 	gen_dffren # (.DW(8)) axi_arlen_dffren (.dnxt(axi_arlen_dnxt), .qout(axi_arlen_qout), .en(axi_arlen_en), .CLK(CLK), .RSTn(RSTn));
 
 
-	wire axi_rlast_set, axi_rlast_rst, axi_rlast_qout;
-	assign axi_rlast_set = ((axi_arlen_cntr == axi_arlen_qout) & ~axi_rlast_qout & axi_arv_arr_flag_qout )  ;
+	assign axi_rlast_set = ((axi_arlen_cnt_qout == axi_arlen_qout) & ~axi_rlast_qout & axi_arv_arr_flag_qout )  ;
 	assign axi_rlast_rst = (~axi_arready_qout & S_AXI_ARVALID & ~axi_arv_arr_flag_qout) | (((axi_arlen_cnt_qout <= axi_arlen_qout) | axi_rlast_qout | ~axi_arv_arr_flag_qout ) & (S_AXI_RREADY));
 	gen_rsffr axi_rlast_rsffr (.set_in(axi_rlast_set), .rst_in(axi_rlast_rst), .qout(axi_rlast_qout), .CLK(CLK), .RSTn(RSTn));
 
 
-	wire [7:0] axi_arlen_cnt_dnxta;
-	wire [7:0] axi_arlen_cnt_dnxtb;
-	wire [7:0] axi_arlen_cnt_qout;
-	wire axi_arlen_cnt_ena;
-	wire axi_arlen_cnt_enb;
 
 	assign axi_arlen_cnt_dnxta = 8'd0;
 	assign axi_arlen_cnt_dnxtb = axi_arlen_cnt_qout + 8'd1;
-	assign axi_arlen_cnt_ena = (~axi_arready & S_AXI_ARVALID & ~axi_arv_arr_flag_qout);
+	assign axi_arlen_cnt_ena = (~axi_arready_qout & S_AXI_ARVALID & ~axi_arv_arr_flag_qout);
 	assign axi_arlen_cnt_enb = ((axi_arlen_cnt_qout <= axi_arlen_qout) & axi_rvalid_qout & S_AXI_RREADY);
 	gen_dpdffren # (.DW(8)) axi_arlen_cnt_dpdffren( .dnxta(axi_arlen_cnt_dnxta), .ena(axi_arlen_cnt_ena), .dnxtb(axi_arlen_cnt_dnxtb), .enb(axi_arlen_cnt_enb), .qout(axi_arlen_cnt_qout), .CLK(CLK), .RSTn(RSTn) );
 
 
 
-	wire axi_rvalid_set, axi_rvalid_rst, axi_rvalid_qout;
 	assign axi_rvalid_set = ~axi_rvalid_qout & axi_arv_arr_flag_qout;
 	assign axi_rvalid_rst =  axi_rvalid_qout & S_AXI_RREADY;
 	gen_rsffr axi_rvalid_rsffr (.set_in(axi_rvalid_set), .rst_in(axi_rvalid_rst), .qout(axi_rvalid_qout), .CLK(CLK), .RSTn(RSTn));
