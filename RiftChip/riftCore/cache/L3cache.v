8013ec4238e5e69980a37281d17a894d0b1d1ff8
Ruige Lee <295054118@qq.com>
2021-03-09 17:43:48 +0800

    L3 cache araddr should not mask in rd

--- RiftChip/riftCore/cache/L3cache.v
+++ RiftChip/riftCore/cache/L3cache.v
@@ -4,7 +4,7 @@
 * @Email: wut.ruigeli@gmail.com
 * @Date:   2021-02-19 10:11:07
 * @Last Modified by:   Ruige Lee
-* @Last Modified time: 2021-03-05 15:49:50
+* @Last Modified time: 2021-03-09 17:42:00
 */
 
 
@@ -503,7 +503,16 @@ assign cache_addr_dnxt =
 	  ( {32{l3c_state_qout == L3C_STATE_CFREE}} & cache_addr_qout )
 	| ( {32{l3c_state_qout == L3C_STATE_CKTAG}} &
 		 
-			(L2C_ARVALID ? (L2C_ARADDR & { {(32-ADDR_LSB){1'b1}}, {ADDR_LSB{1'b0}} }) : (db_full ? db_addr_o : (L2C_AWADDR & ( l3c_state_dnxt == L3C_STATE_RSPWR ? ~32'h0 : { {(32-ADDR_LSB){1'b1}}, {ADDR_LSB{1'b0}} } )) ) )
+			( 
+			  	{32{L2C_ARVALID}} & L2C_ARADDR & ( l3c_state_dnxt == L3C_STATE_RSPRD ? ~32'h0 : { {(32-ADDR_LSB){1'b1}}, {ADDR_LSB{1'b0}} } )
+			)
+			|
+
+			(
+				{32{L2C_AWVALID}} & 
+				 ( {32{ db_full}} & db_addr_o )
+				|( {32{~db_full}} & L2C_AWADDR & ( l3c_state_dnxt == L3C_STATE_RSPWR ? ~32'h0 : { {(32-ADDR_LSB){1'b1}}, {ADDR_LSB{1'b0}} } ) )
+			)
 	  )
 	| ( {32{l3c_state_qout == L3C_STATE_FENCE}} & db_addr_o )
 	| ( {32{l3c_state_qout == L3C_STATE_EVICT}} & ((MEM_WVALID & MEM_WREADY) ? cache_addr_qout + 32'b1000 : cache_addr_qout) )
